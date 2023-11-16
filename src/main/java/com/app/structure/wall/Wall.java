0c5c75eca9c80b4be3e23e2596079b2e972cfede
Filip <pietrasfilip2@gmail.com>
2023-11-16 20:07:39 +0100

    Filled methods inside Wall class with logic. Added two auxiliary methods.

--- src/main/java/com/app/structure/wall/Wall.java
+++ src/main/java/com/app/structure/wall/Wall.java
@@ -1,12 +1,16 @@
 package com.app.structure.wall;
 
 import com.app.block.Block;
+import com.app.block.composite_block.CompositeBlock;
 import com.app.structure.Structure;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 
 @Builder
@@ -14,18 +18,66 @@ import java.util.Optional;
 public class Wall implements Structure {
     private List<Block> blocks;
 
+    /**
+     *
+     * @param color block color
+     * @return any block that are in given color
+     */
     @Override
     public Optional<Block> findBlockByColor(String color) {
-        return Optional.empty();
+        return findBlockByPredicate(block -> block.getColor().equals(color))
+                .parallelStream()
+                .findAny();
     }
 
+    /**
+     *
+     * @param material block material
+     * @return all blocks that are made of given material
+     */
     @Override
     public List<Block> findBlocksByMaterial(String material) {
-        return null;
+        return findBlockByPredicate(block -> block.getMaterial().equals(material));
     }
 
+    /**
+     *
+     * @return number of blocks, that creates structure
+     */
     @Override
     public int count() {
-        return 0;
+        return findBlockByPredicate(Objects::nonNull).size();
+    }
+
+    /**
+     *
+     * @param predicate predicate condition
+     * @return - list of blocks, that met given condition
+     */
+    private List<Block> findBlockByPredicate(Predicate<Block> predicate) {
+        var result = new ArrayList<Block>();
+
+        for (var block : blocks) {
+            checkComposite(block, predicate, result);
+        }
+        return result;
+    }
+
+    /**
+     *
+     * @param block element from blocks list. Can be Block or CompositeBlock.
+     * @param predicate predicate condition.
+     * @param result list with final result.
+     */
+    private void checkComposite(Block block, Predicate<Block> predicate, List<Block> result) {
+        if (predicate.test(block)) {
+            result.add(block);
+        }
+
+        if (block instanceof CompositeBlock compositeBlock) {
+            for (var composite : compositeBlock.getBlocks()) {
+                checkComposite(composite, predicate, result);
+            }
+        }
     }
 }
